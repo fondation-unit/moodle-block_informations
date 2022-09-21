d4b167832fc9d9be6ae1a8143290b5c63352cc7e
Hipjea <pierre.duverneix@gmail.com>
2022-09-21 14:55:57 +0200

    default image setting

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022092103;
+$plugin->version = 2022092104;
 $plugin->requires = 2016052300;
 $plugin->release = '1.0.0';
 $plugin->component = 'block_informations';
