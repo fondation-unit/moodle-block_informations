fd264d4e5c12138c5b274c78c09b650f8cd01f59
Hipjea <pierre.duverneix@gmail.com>
2022-09-21 11:21:09 +0200

    add configure context

--- configure.php
+++ configure.php
@@ -42,9 +42,12 @@ if ($configurelicenceparam && $configureimageparam) {
     block_informations_add_licence($licence);
 }
 
+$systemcontext = context_system::instance();
 $pageurl = new moodle_url('/blocks/informations/configure.php');
 $PAGE->set_url($pageurl);
 $PAGE->set_title("{$SITE->shortname}: " . get_string('configuration', 'block_informations'));
+$PAGE->set_context($systemcontext);
+$PAGE->navbar->add(get_string('pluginname', 'block_informations'));
 $output = $PAGE->get_renderer('block_informations');
 
 
